e1fcd4b0e97704de4c79292bde04ebc0968e1ce8
Dave Wood <dave@cerebralgardens.com>
2022-12-14 16:14:11 -0500

    feat: add `ThemeDataPrime` extension with a quick method to generate a derived `CupertinoThemeData` object

--- lib/flutter_prime.dart
+++ lib/flutter_prime.dart
@@ -4,6 +4,7 @@ library flutter_prime;
 
 export 'src/extensions/brightness.dart';
 export 'src/extensions/color.dart';
+export 'src/extensions/theme_data.dart';
 export 'src/flutter_prime_base.dart';
 export 'src/widgets/empty_icon.dart';
 export 'src/widgets/empty_view.dart';
---
+++ lib/src/extensions/theme_data.dart
@@ -0,0 +1,30 @@
+library flutter_prime;
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// ignore: unused_import
+import 'package:flutter_prime/flutter_prime.dart';
+
+extension ThemeDataPrime on ThemeData {
+  CupertinoThemeData get cupertinoThemeData {
+    return CupertinoThemeData(
+      brightness: brightness,
+      primaryColor: colorScheme.primary,
+      primaryContrastingColor: colorScheme.onPrimary,
+      textTheme: CupertinoTextThemeData(
+        primaryColor: colorScheme.primary,
+        textStyle: textTheme.bodyMedium,
+        actionTextStyle: textTheme.displaySmall,
+        tabLabelTextStyle: textTheme.titleSmall,
+        navTitleTextStyle: textTheme.titleSmall,
+        navLargeTitleTextStyle: textTheme.titleMedium,
+        navActionTextStyle: textTheme.titleSmall,
+        pickerTextStyle: textTheme.displaySmall,
+        dateTimePickerTextStyle: textTheme.displaySmall,
+      ),
+      barBackgroundColor: colorScheme.primary,
+      scaffoldBackgroundColor: colorScheme.background,
+    );
+  }
+}
