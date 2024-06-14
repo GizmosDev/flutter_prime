3246809b76a60543775db8296fa1ff03f040d19a
Dave Wood <dave@cerebralgardens.com>
2024-06-14 14:36:46 -0400

    chore: switches from deprecated `SchedulerBinding.instance.window` to `SchedulerBinding.instance.platformDispatcher`

--- lib/src/extensions/brightness.dart
+++ lib/src/extensions/brightness.dart
@@ -9,7 +9,7 @@ import 'package:flutter_prime/flutter_prime.dart';
 extension BrightnessPrime on Brightness {
   /// Return the current platform [Brightness] setting, without a [BuildContext].
   static Brightness get platformBrightness {
-    return SchedulerBinding.instance.window.platformBrightness;
+    return SchedulerBinding.instance.platformDispatcher.platformBrightness;
   }
 
   /// Return a [bool] indicating whether `this` Brightness matches the current platform [Brightness] setting.
